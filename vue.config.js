f9cf4c402897d2c829c26bd398f5b61031e1feea
wari-1 <405770221@qq.com>
2019-09-19 10:57:46 +0800

    a

--- vue.config.js
+++ vue.config.js
@@ -1,30 +1,48 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "/vue-kuayu/" : "/",
+  // publicPath: process.env.NODE_ENV === "production" ? "/vue-kuayu/" : "/",
+  runtimeCompiler: true,
+  publicPath: "/vue-kuayu/",
   //配置代理服务器
   devServer: {
+    // open: process.platform === 'darwin',
+    // host: 'localhost',
+    port: 8080,
+    // open: true, //配置自动启动浏览器
     proxy: {
       "/api": {
-        target: "https://www.wanandroid.com",
-        ws: true,
+        target: "https://www.wanandroid.com", //对应自己的接口
         changeOrigin: true,
+        ws: true,
         pathRewrite: {
           "^/api": ""
         }
       }
     }
   },
+  // devServer: {
+  //   proxy: {
+  //     "/api": {
+  //       target: "https://www.wanandroid.com",
+  //       ws: true,
+  //       changeOrigin: true,
+  //       pathRewrite: {
+  //         "^/api": ""
+  //       }
+  //     }
+  //   }
+  // },
 
   css: {
     loaderOptions: {
       stylus: {
-        'resolve url': true,
-        'import': []
+        "resolve url": true,
+        import: []
       }
     }
   },
 
   pluginOptions: {
-    'cube-ui': {
+    "cube-ui": {
       postCompile: true,
       theme: false
     }
